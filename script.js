ea84b12eb8ebe897110306efd27078ee2731dbdc
Hódi Kristóf <kristof.hodi@gmail.com>
2025-09-09 12:28:22 +0200

    scriptttt

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const apiURL = "https://publicapis.io/jokes-api";
 
 const viccHelye = document.getElementById("viccHelye");
 const randomVicc = document.getElementById("randomVicc");
+
+async function getRandomJoke() {
+  try {
+    const URL = "https://official-joke-api.appspot.com/jokes/random"
+    const response = await fetch(URL);
+    if (!response.ok) {
+      throw new Error(`Api válasza: ${response.status}`);
+    }
+
+    const result = await response.json();
+    viccHelye.innerHTML = "Itt a vicc: " + result.setup + " " + result.punchline;
+  } catch (error) {
+    viccHelye.innerHTML = error;
+  }
+}
