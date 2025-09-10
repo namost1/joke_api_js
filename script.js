5734624e03112e997adc3a362f712897d822061d
Hódi Kristóf <kristof.hodi@gmail.com>
2025-09-10 11:03:36 +0200

    id script

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ async function getRandomJoke() {
     viccHelye.innerHTML = error;
   }
 }
+
+async function getRandomJokeById() {
+  try {
+    const viccId = document.getElementById("viccid").value
+    const URL = `https://official-joke-api.appspot.com/jokes/${viccId}`
+    const response = await fetch(URL);
+    if (!response.ok) {
+      throw new Error(`Api válasza: ${response.status}`);
+    }
+
+    const result = await response.json();
+    viccHelye.innerHTML = "Itt a vicc: " + result.setup + " " + result.punchline;
+  } catch (error) {
+    viccHelye.innerHTML = error;
+  }
+}
